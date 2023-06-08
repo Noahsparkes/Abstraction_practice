1be8a480622e85e47a634af6adf9d52b61bb341a
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-06-08 15:52:11 +0200

    abstraction complete.

--- scripts.js
+++ scripts.js
@@ -2,21 +2,17 @@ function Employee(name, age, baseSalary){
     this.name = name;
     this.age = age;
     this.baseSalary = baseSalary;
-    this.monthlyBonus = 1000; // should be made internal with abstraction.
+    let monthlyBonus = 1000; 
 
-   this.calculateFinalSalary = function(){ 
-     let finalSalary = this.baseSalary + this.monthlyBonus;
+   let calculateFinalSalary = function(){ 
+     let finalSalary = baseSalary + monthlyBonus;
      console.log('Final salary is : ' + finalSalary);
-   } // This function should also be made internal with abstraction.
-
+   } 
 
     this.getEmpDetails = function(){
         console.log('Name : ' +this.name+' | Age :'+this.age);
+        calculateFinalSalary();
     }
 }
 
 let emp1 = new Employee('John', 30, 2000);
-emp1.getEmpDetails();
-emp1.calculateFinalSalary();
-
- 
