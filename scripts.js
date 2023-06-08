26528514b16ec5b049004ab3cd55061cd8f5cc9a
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-06-08 15:47:58 +0200

    identifying parts that should be made internal.

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ function Employee(name, age, baseSalary){
     this.name = name;
     this.age = age;
     this.baseSalary = baseSalary;
-    this.monthlyBonus = 1000;
+    this.monthlyBonus = 1000; // should be made internal with abstraction.
 
-   this.calculateFinalSalary = function(){
+   this.calculateFinalSalary = function(){ 
      let finalSalary = this.baseSalary + this.monthlyBonus;
      console.log('Final salary is : ' + finalSalary);
-   }
+   } // This function should also be made internal with abstraction.
 
 
     this.getEmpDetails = function(){
@@ -17,4 +17,6 @@ function Employee(name, age, baseSalary){
 
 let emp1 = new Employee('John', 30, 2000);
 emp1.getEmpDetails();
+emp1.calculateFinalSalary();
 
+ 
